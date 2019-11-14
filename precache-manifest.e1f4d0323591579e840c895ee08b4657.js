cf93858abd2f0bd0fe6c6fc5bdb8fcc6496afffa
mojTaba shayegh <shayegh@gmail.com>
2019-11-14 16:40:54 +0330

    Updates

---
+++ precache-manifest.e1f4d0323591579e840c895ee08b4657.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de639e8e91a01a401fed4203c3cc0d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1aeffbf7319663cd4633",
+    "url": "/monsters-rolodex/static/css/main.2111b3b1.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "1aeffbf7319663cd4633",
+    "url": "/monsters-rolodex/static/js/main.21354a67.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
