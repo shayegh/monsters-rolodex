2b9045cd8efa01c952b3b072ba8644e662fdc04c
mojTaba shayegh <shayegh@gmail.com>
2019-11-14 16:31:08 +0330

    Project Completed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,6 +4,8 @@ import "./card-list.styles.css";
 import Card from './../card/card.component';
 
 export const CardList = props => {
+   
+    
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
